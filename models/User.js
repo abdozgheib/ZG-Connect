bcb4659fdcf314772bdbb4cf1f441a22f280ca09
Abdo <abdozgheib8@gmail.com>
2026-05-15 12:11:16 +0300

    fixed avatar saving in MongoDB

--- models/User.js
+++ models/User.js
@@ -4,8 +4,12 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
+  phone: { type: String, default: '' },
+  avatar: { type: String, default: '' },
+  about: { type: String, default: 'Hey there! I am using ZG Connect.' },
+  contacts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   online: { type: Boolean, default: false },
   lastSeen: { type: Date, default: Date.now }
-}, { timestamps: true });
+}, { timestamps: true, strict: false });
 
 module.exports = mongoose.model('User', userSchema);
